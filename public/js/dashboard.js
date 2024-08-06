ce5c1b33747d604c53517ffeb641a198d474e7c1
Virgi <virgiriyadi2404@gmail.com>
2024-08-06 21:25:04 +0700

    Update dashboard.js
    Add ajax request for handling a live search

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,4 +1,4 @@
-// public/js/custom.js
+// public/js/dashboard.js
 
 $(document).ready(function() {
     $('#saveTemplateBtn').on('click', function(e) {
@@ -20,7 +20,64 @@ $(document).ready(function() {
         $('#pesan').val(selectedTemplate);
         $('#nama_template').val(templateName);
     });
-    
 
+    $('#search').on('keyup', function() {
+        let query = $(this).val();
+
+        $.ajax({
+            url: search,
+            type: "GET",
+            data: { 'query': query },
+            success: function(data) {
+                $('#pegawai-list').empty();
+                if (data.length > 0 && query.length > 0) {
+                    data.forEach(pegawai => {
+                        let pegawaiHtml = `
+                            <div type="button" class="btn btn-outline-success rounded p-2 text-start d-flex justify-content-between align-items-center">
+                                ${pegawai.nama}
+                                <div class="btn-group">
+                                    <button type="button" class="btn btn-sm btn-secondary rounded" data-bs-toggle="dropdown" aria-expanded="false" aria-label="dropdown">
+                                        <i class="fa-solid fa-ellipsis"></i>
+                                    </button>
+                                    <ul class="dropdown-menu">
+                                        <li><a class="dropdown-item" href="/arsip">Arsip Pesan</a></li>
+                                        <li><hr class="dropdown-divider"></li>
+                                        <li><a class="dropdown-item" href="/editpegawai/${pegawai.id}">Edit</a></li>
+                                        <li><hr class="dropdown-divider"></li>
+                                        <li class="dropdown-item" data-bs-toggle="modal" data-bs-target="#Hapus${pegawai.nip}">Hapus</li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div class="modal fade" id="Hapus${pegawai.nip}" tabindex="-1" aria-labelledby="HapusLabel" aria-hidden="true">
+                                <div class="modal-dialog">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h1 class="modal-title fs-5" id="HapusLabel">Hapus Data</h1>
+                                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                        </div>
+                                        <div class="modal-body text-center">
+                                            Apakah anda yakin ingin menghapus data ini?<br>
+                                            <b>${pegawai.nama}</b>
+                                        </div>
+                                        <div class="modal-footer">
+                                            <form action="/hapuspegawai/${pegawai.id}" method="POST">
+                                                <input type="hidden" name="_token" value="${$('meta[name="csrf-token"]').attr('content')}">
+                                                <input type="hidden" name="_method" value="DELETE">
+                                                <button type="submit" class="btn btn-danger">Hapus</button>
+                                            </form>
+                                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Kembali</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        `;
+                        $('#pegawai-list').append(pegawaiHtml);
+                    });
+                } else {
+                    $('#pegawai-list').empty();
+                    $('#pegawai-list').append('<h2 class="text-secondary opacity-75 text-center">Pencarian Kosong</h2>');
+                }
+            }
+        });
+    });
 });
-
